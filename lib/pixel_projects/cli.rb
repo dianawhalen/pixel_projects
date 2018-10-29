0c4b952dfb0da92ddf8fe5b43a2f726eed05cfe8
dianawhalen <whalendianaj@gmail.com>
2018-10-28 20:10:23 -0600

    add CLI Controller call and greeting methods

---
+++ lib/pixel_projects/cli.rb
@@ -0,0 +1,16 @@
+class PixelProjects::CLI
+
+  def call
+    greeting
+  end
+
+  def greeting
+    puts "⭑-------------------------------------------------------------⭑"
+    puts "                                                               "
+    puts "           ¸,ø¤º°`°º¤ø,¸¸,ø¤º°º¤ø,¸¸,ø¤º°`°º¤ø,¸               "
+    puts "      ¸,ø¤º°`°º¤ø,¸¸,ø¤º° WELCOME °º¤ø,¸¸,ø¤º°`°º¤ø,¸          "
+    puts "           ¸,ø¤º°`°º¤ø,¸¸,ø¤º°º¤ø,¸¸,ø¤º°`°º¤ø,¸               "
+    puts "                                                               "
+    puts "      Hold tight for the most recent pixel creations!!         "
+    puts "⭑-------------------------------------------------------------⭑"
+  end
