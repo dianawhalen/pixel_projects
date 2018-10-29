106ab6caddbbb279eaf8bfa8ad4d6253e18d49ec
dianawhalen <whalendianaj@gmail.com>
2018-10-28 19:36:31 -0600

    add files required

--- lib/pixel_projects.rb
+++ lib/pixel_projects.rb
@@ -1,5 +1,9 @@
-require "pixel_projects/version"
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
-module PixelProjects
-  # Your code goes here...
-end
+require_relative './pixel_projects/cli'
+require_relative './pixel_projects/designer'
+require_relative './pixel_projects/dribbble'
+require_relative './pixel_projects/scraper'
+require_relative './pixel_projects/version'
