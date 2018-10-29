29a437204cfec143bead94518092ce17e29abccd
dianawhalen <whalendianaj@gmail.com>
2018-10-28 20:32:23 -0600

    add Designer initialize method

---
+++ lib/pixel_projects/designer.rb
@@ -0,0 +1,10 @@
+class PixelProjects::Designer
+  attr_accessor :title, :comment, :shot_url, :designer_url, :designer_name, :name, :location, :bio, :skills, :teams, :profile_url, :web
+
+  @@all = []
+
+  def initialize(designers)
+    designers.each {|key, value| self.send(("#{key}="), value) }
+    @@all << self
+  end
+end
